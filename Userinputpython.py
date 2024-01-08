431aca2db5b61cc1194d25bc7364b66e4b2226a9
Avaneesh verma <124239728+avaneeshverma850@users.noreply.github.com>
2024-01-08 12:08:49 +0530

    Add files via upload

---
+++ Userinputpython.py
@@ -0,0 +1,46 @@
+#Simple intrest
+"""print("Enter principle,rate and time")
+a=int(input())
+b=float(input())
+c=int(input())
+SI=(a*b*c)/100
+print("Simple intrest is:",SI)"""
+#Area of Rectangle
+"""print("Enter length and Breadth")
+l=int(input())
+b=int(input())
+Area=l*b
+print("Area Of Recangle is:",Area)"""
+#Average of number
+"""print("Enter three numbers")
+a=int(input())
+b=int(input())
+c=int(input())
+Average=(a+b+c)/3
+print("Average:",Average)"""
+#Volume of Cubeoid
+"""print("Enter length,Breadth,height")
+a=int(input())
+b=int(input())
+c=int(input())
+Volume=a*b*c
+print("Volume Of cubeoid is:",Volume)"""
+#Calculate power
+"""print("Enter 2 number")
+x=int(input())
+y=int(input())
+Result=x**y
+print("Result:",Result)"""
+
+
+
+
+
+
+
+
+
+
+
+
+
